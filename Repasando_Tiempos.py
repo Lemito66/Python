11b7038029ac2e6a85ad6ade29ba067e6e13eaf5
Lemito66 <lemito66@gmail.com>
2021-02-10 18:50:41 -0500

    Reapasando a los tiempos con listas y numero mayor

--- Repasando_Tiempos.py
+++ Repasando_Tiempos.py
@@ -1,9 +1,19 @@
-numeroUno=input("Ingrese el primer numero: ")
-numeroDos=input("Ingrese el segundo numero: ")
+def numeroMayor():
+    print("Escoje uno de estos numeros")
+    numeroUno=input("Ingrese el primer numero: ")
+    numeroDos=input("Ingrese el segundo numero: ")
+    if numeroUno > numeroDos:
+        print("El numero "+ numeroUno +" es mayor al "+ numeroDos)
+    elif numeroDos>numeroUno:
+        print("El número "+ numeroDos+" es mayor al " + numeroUno)
+    else:
+        print("Son iguales")
 
-if numeroUno>numeroDos:
-    print ("El numero " + numeroDos +" es mayor al " +numeroDos)
-elif numeroDos>numeroUno:
-    print("El numero"+ numeroDos +" es mayor al "+numeroUno)
-else:
-    print("Son iguales")
+def Lista():
+    lista=[10,20,30,40,50,60,70]
+    lista.extend([80])
+    print(lista)
+
+Lista()
+numeroMayor()
+
